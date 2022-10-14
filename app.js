74beb7f4e62c4f00fa7e91c5ade34d5938eb733f
opensource.nikola <110063322+opensourcenikola@users.noreply.github.com>
2022-10-14 22:50:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-let validation_key = document.getElementById('key').value;
+let validation_key, element = document.getElementById('key');
 let form = document.getElementById('form');
 let error = document.getElementById('error');
 
-function acceptValidationKey() {
+if (element != null) {
+    validation_key = element;
+}
+
 form.addEventListener('submit', (e) => {
-    let messages = []
-        if(validation_key !== "265nh43aar39o9p00tz5er678sd9q") {
-            messages.push('Wrong validation key.');
+        let messages = [];
+        if(validation_key.value !== "265nh43aar39o9p00tz5er678sd9q") {
+            messages.push('Wrong validation key');
         }
-        else {
-            window.location.href = 'nikolium.html';
-        }
-        if(messages > 0) {
+        if(messages.length > 0) {
             e.preventDefault();
             error.innerText = messages;
         }
-})
-}
+});
